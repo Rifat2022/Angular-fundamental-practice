8a613c20f6496bc3dda2a23c588b675b0403436f
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-11-19 13:11:20 +0600

    communication with child component exercise is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventAppComponent } from './event-app.component';
+import { EventAddressComponent } from './events/event-address.component';
+import { EventDetailsComponent } from './events/event-details.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventAppComponent
+    EventAppComponent, 
+    EventDetailsComponent, 
+    EventAddressComponent
   ],
   providers: [
     
