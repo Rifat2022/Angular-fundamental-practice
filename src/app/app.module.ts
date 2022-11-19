5e61c282ed7840a0a78152c61b073ff1b5f99307
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-11-19 12:28:57 +0600

    all angular files are added to github

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventAppComponent } from './event-app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventAppComponent
+  ],
+  providers: [
+    
+  ],
+
+  bootstrap: [EventAppComponent]
+})
+export class AppModule { }
---
+++ src/app/event-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './event-app.component.html',
+
+})
+export class EventAppComponent {
+ 
+}
