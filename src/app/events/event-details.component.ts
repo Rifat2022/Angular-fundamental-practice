fe1710079a7d6fc9cfbb30d8d307b394c9cf5b91
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-11-19 18:16:09 +0600

    hiding and showing elements with ngSwitch is done

--- src/app/events/event-details.component.ts
+++ src/app/events/event-details.component.ts
@@ -17,11 +17,10 @@ export class EventDetailsComponent{
     toggleDisplay(){
         this.displayDetails = !this.displayDetails
     }
-    events = [
-        {name:'Angular Connect', date: '9/26/2036', time: '10am', location: {address: '1 London Road ', city: 'London', country: 'England'}},
-        {name:'ng-conf 2037', date: '4/15/2037', time: '9am', onlineUrl: 'https://www.ng-conf.org/'},
+   events = [
+        {name:'Angular Connect', date: '9/26/2036', time: '10am', location: {address: '1 London Road ', city: 'London', country: 'England'}, format:"InPerson"},
+        {name:'ng-conf 2037', date: '4/15/2037', time: '9am', onlineUrl: 'https://www.ng-conf.org/', format:"Online"},
         {name:'Future Conf (Location/Url TBD)', date: '6/10/2037', time: '8am'},
-        {name:'ng-nl', date: '4/15/2037', time: '9am', onlineUrl:'http://ng-nl.org/'},
-        {name:'UN Angular Summit', date: '6/10/2037', time: '8am', location: {address: 'The UN Angular Center', city: 'New York', country: 'USA'}, onlineUrl: 'http://unangularsummit.org'}
-      ]
+        {name:'UN Angular Summit', date: '6/10/2037', time: '8am', location: {address: 'The UN Angular Center', city: 'New York', country: 'USA'}, format:"InPerson"}
+      ] 
 }
