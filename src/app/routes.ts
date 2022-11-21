a31ae4c8cf4dc21f80ebd8a78bfcc7b98e183521
Rifat2022 <md.rifathossainshaon@gmail.com>
2022-11-21 16:18:31 +0600

    upto navigation from page is done

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,9 @@ import {Routes} from '@angular/router'
 import { EventDetailsComponent } from './event-details.component'
 import { EventsListComponent } from './events-list.component'
 export const appRoute: Routes = [
-    {path: '', component: EventsListComponent}, 
-    {path: 'events/:id' , component: EventDetailsComponent}
+    
+    {path: 'events', component: EventsListComponent}, 
+    {path: 'events/:id' , component: EventDetailsComponent}, 
+    {path: '', redirectTo:'events', pathMatch: 'full'}, 
+    
 ]
